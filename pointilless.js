ca10e4a9745adb595a9b078a5f4c6c1da78f6eab
Quinn Schiller <SchillerQuinn@gmail.com>
2019-02-12 15:58:17 -0600

    testing 123

--- pointilless.js
+++ pointilless.js
@@ -6,116 +6,119 @@ var max_particles = 100;
 var all_particles = new particleSystem();
 var button;
 
-function setup() {
-    var canvas = createCanvas(
-        window.innerWidth,
-        window.innerHeight
-      );
-    canvas.parent("sketch");
-    background(0);
+let img;
+let imgDict
 
-    // button = createButton('click me');
-    // // put button in same container as the canvas
-    // button.parent("sketch");
-    // // by default this sets position relative to window...
-    // button.position(0, 0);
+function preload() {
+  img = loadImage('Sarina.jpg');
+}
+
+
+function setup() {
+  var canvas = createCanvas(
+    1280,
+    1232
+  );
+  canvas.parent("sketch");
+  background(0);
+  imageMode(CENTER);
+  noStroke();
+  background(255);
+  img.loadPixels();
+  imgDict = img.get()
+
+  // button = createButton('click me');
+  // // put button in same container as the canvas
+  // button.parent("sketch");
+  // // by default this sets position relative to window...
+  // button.position(0, 0);
 
 }
 
 function draw() {
-    all_particles.update();
+  all_particles.update();
 }
 
 function mouseDragged() {
-    if (all_particles.count < max_particles) {
-        all_particles.addParticle(createVector(mouseX, mouseY));
-    }
+  if (all_particles.count < max_particles) {
+    all_particles.addParticle(createVector(mouseX, mouseY));
+  }
 }
 
 function particleSystem() {
-    this.count = 0;
-    this.wanderers = [];
+  this.count = 0;
+  this.wanderers = [];
 
-    this.addParticle = function(location) {
-        this.count++;
-        append(this.wanderers, new Particle(location));
-    };
+  this.addParticle = function(location) {
+    this.count++;
+    append(this.wanderers, new Particle(location));
+  };
 
-    this.update = function() {
-        for(var i=0; i < this.wanderers.length; i++) {
-            this.wanderers[i].update();
-            if (this.wanderers[i].isDead()) {
-                this.wanderers.splice(i, 1);
-                this.count--;
-            }
-            else {
-                this.wanderers[i].display();
-            }
+  this.update = function() {
+    for (var i = 0; i < this.wanderers.length; i++) {
+      this.wanderers[i].update();
+      if (this.wanderers[i].isDead()) {
+        this.wanderers.splice(i, 1);
+        this.count--;
+      } else {
+        this.wanderers[i].display();
+      }
 
-        }
-    };
+    }
+  };
 }
 
 class Particle {
-    constructor(location) {
-        this.location = createVector(location.x, location.y);
-        this.size = 100;
-        this.velocity = createVector(0, 0);
-        this.acc = createVector(0, 0);
-        this.angle = 0.0
+  constructor(location) {
+    this.location = createVector(location.x, location.y);
+    this.size = 1;
+    this.velocity = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.angle = 0.0
 
-        this.color = new colorGenerator();
+    this.color = new colorGenerator(this.location);
+  }
+
+  update(location) {
+    this.angle += random(0, TWO_PI);
+    var magnitude = random(0, 4);
+    this.acc.x += cos(this.angle) * magnitude;
+    this.acc.y += sin(this.angle) * magnitude;
+    this.acc.limit(3);
+    this.velocity.add(this.acc);
+    this.velocity.limit(6);
+    this.location.add(this.velocity);
+    this.size += shrink_rate;
+  }
+
+  display() {
+    this.color.update(this.location)
+    fill(this.color.R, this.color.G, this.color.B, 255 - this.size * 5);
+    noStroke();
+    ellipse(this.location.x, this.location.y, this.size);
+  }
+
+  isDead() {
+    if (this.size > 50) {
+      return true;
+    } else {
+      return false;
     }
-
-    update() {
-        this.angle += random(0, TWO_PI);
-        var magnitude = random(0, 4);
-
-        this.acc.x += cos(this.angle) * magnitude;
-        this.acc.y += sin(this.angle) * magnitude;
-        this.acc.limit(3);
-
-        this.velocity.add(this.acc);
-        this.velocity.limit(6);
-
-        this.location.add(this.velocity);
-
-        this.size -= shrink_rate;
-    }
-
-    display() {
-        this.color.update();
-        fill(this.color.R, this.color.G, this.color.B);
-        stroke(0, 125);
-        ellipse(this.location.x, this.location.y, this.size);
-    }
-
-    isDead() {
-        if (this.size < 6) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
+  }
 }
 
 class colorGenerator {
-    constructor() {
-        this.min_speed = 0.2;
-        this.max_speed = 0.7;
-        this.R = random(255);
-        this.G = random(255);
-        this.B = random(255);
-
-        this.Rspeed = (random(1) > 0.5 ? 1 : -1) * random(this.min_speed, this.max_speed);
-        this.Gspeed = (random(1) > 0.5 ? 1 : -1) * random(this.min_speed, this.max_speed);
-        this.Bspeed = (random(1) > 0.5 ? 1 : -1) * random(this.min_speed, this.max_speed);
+  constructor(loc) {
+    console.log(loc)
+    this.R = red(img.get(loc.x, loc.y))
+    this.G = green(img.get(loc, loc.y))
+    this.B = blue(img.get(loc.x, loc.y)
     }
 
-    update() {
-        this.Rspeed = ((this.R += this.Rspeed) > 255 || (this.R < 0)) ? -this.Rspeed : this.Rspeed;
-        this.Gspeed = ((this.G += this.Gspeed) > 255 || (this.G < 0)) ? -this.Gspeed : this.Gspeed;
-        this.Bspeed = ((this.B += this.Bspeed) > 255 || (this.B < 0)) ? -this.Bspeed : this.Bspeed;
+    update(loc) {
+      console.log(loc)
+      this.R = red(img.get(loc.x, loc.y))
+      this.G = green(img.get(loc.x, loc.y))
+      this.B = blue(img.get(loc.x, loc.y))
     }
-}
+  }
