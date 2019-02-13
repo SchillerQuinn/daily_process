b5c4db5286567adbe77f0ec210c382305ec38738
Quinn Schiller <SchillerQuinn@gmail.com>
2019-02-13 12:40:23 -0600

    Revert "Larger particles when you shake it"
    This reverts commit 07eb1ff7c5798275a0951bd12db026e8e11da862.

--- pointilless.js
+++ pointilless.js
@@ -7,21 +7,21 @@ var all_particles = new particleSystem();
 var button;
 let scale;
 
-let imgg, //imag     //resized image of the image (visible)
-  imageRatio; //ratio of the image h/w
+let imgg,       //imag     //resized image of the image (visible)
+    imageRatio;     //ratio of the image h/w
 
 function preload() {
-  imgNum = floor(random(1, 33))
-  console.log(imgNum)
-  imgg = loadImage(imgNum + ".jpg");
+    imgNum = floor(random(1,33))
+    console.log(imgNum)
+    imgg=loadImage(imgNum+".jpg");
 }
 
 
 function setup() {
-  scale = min(windowHeight / imgg.height, windowWidth / imgg.width)
-  shrink_rate = min(windowHeight / 300, windowWidth / 300);
+  scale =min(windowHeight/ imgg.height, windowWidth/ imgg.width)
+  shrink_rate = min(windowHeight/300, windowWidth/300);
   console.log(scale)
-  canvas = createCanvas(imgg.width * scale, imgg.height * scale);
+  canvas = createCanvas(imgg.width*scale, imgg.height*scale);
   canvas.parent("sketch");
   background(0);
   imageMode(CENTER);
@@ -37,7 +37,6 @@ function setup() {
   // button.position(0, 0);
 
 }
-
 function draw() {
   //all_particles = new particleSystem();
   all_particles.update();
@@ -45,31 +44,30 @@ function draw() {
 
 function mousePressed() {
   if (all_particles.count < max_particles) {
-    all_particles.addParticle(createVector(mouseX, mouseY), 20);
+    all_particles.addParticle(createVector(mouseX, mouseY));
   }
 }
 
 function mouseMoved() {
   if (all_particles.count < max_particles) {
-    all_particles.addParticle(createVector(mouseX, mouseY), 20);
+    all_particles.addParticle(createVector(mouseX, mouseY));
   }
 }
 
 
 function deviceShaken() {
   if (all_particles.count < max_particles) {
-    //create 5 particles and make them slightly larger than normal
-    all_particles.addParticle(createVector(random(0, imgg.width * scale), random(0, imgg.height * scale)), 10);
-    all_particles.addParticle(createVector(random(0, imgg.width * scale), random(0, imgg.height * scale)), 10);
-    all_particles.addParticle(createVector(random(0, imgg.width * scale), random(0, imgg.height * scale)), 10);
-    all_particles.addParticle(createVector(random(0, imgg.width * scale), random(0, imgg.height * scale)), 10);
-    all_particles.addParticle(createVector(random(0, imgg.width * scale), random(0, imgg.height * scale)), 10);
-  }
+  all_particles.addParticle(createVector(random(0,imgg.width*scale), random(0,imgg.height*scale)));
+  all_particles.addParticle(createVector(random(0,imgg.width*scale), random(0,imgg.height*scale)));
+  all_particles.addParticle(createVector(random(0,imgg.width*scale), random(0,imgg.height*scale)));
+  all_particles.addParticle(createVector(random(0,imgg.width*scale), random(0,imgg.height*scale)));
+  all_particles.addParticle(createVector(random(0,imgg.width*scale), random(0,imgg.height*scale)));
+ }
 }
 
 function touchMoved() {
   if (all_particles.count < max_particles) {
-    all_particles.addParticle(createVector(mouseX, mouseY), 20);
+    all_particles.addParticle(createVector(mouseX, mouseY));
   }
   return false;
 }
@@ -77,7 +75,7 @@ function touchMoved() {
 
 function mouseDragged() {
   if (all_particles.count < max_particles) {
-    all_particles.addParticle(createVector(mouseX, mouseY), 20);
+    all_particles.addParticle(createVector(mouseX, mouseY));
   }
 }
 
@@ -105,10 +103,9 @@ function particleSystem() {
 }
 
 class Particle {
-  constructor(location, scaleAmount) {
+  constructor(location) {
     this.location = createVector(location.x, location.y);
-    this.isize = min(windowHeight / scaleAmount, windowWidth / scaleAmount);
-    this.size = min(windowHeight / scaleAmount, windowWidth / scaleAmount);
+    this.size = min(windowHeight/20, windowWidth/20);
     this.velocity = createVector(0, 0);
     this.acc = createVector(0, 0);
     this.angle = 0.0
@@ -125,26 +122,26 @@ class Particle {
     this.velocity.add(this.acc);
     this.velocity.limit(6);
     this.location.add(this.velocity);
-    this.size = this.size * 0.9 - 0.01
+    this.size = this.size*0.9-0.01
   }
 
   display() {
     this.color.update(this.location)
-    fill(this.color.R, this.color.G, this.color.B, 255 - 255 * (this.size / this.isize));
-    this.x1 = this.location.x + Math.cos(this.angle + (2 * Math.PI / 3)) * this.size / 2
-    this.y1 = this.location.y + Math.sin(this.angle + (2 * Math.PI / 3)) * this.size / 2
-    this.x2 = this.location.x + Math.cos(this.angle + 2 * (2 * Math.PI / 3)) * this.size / 2
-    this.y2 = this.location.y + Math.sin(this.angle + 2 * (2 * Math.PI / 3)) * this.size / 2
-    this.x3 = this.location.x + Math.cos(this.angle) * this.size / 2
-    this.y3 = this.location.y + Math.sin(this.angle) * this.size / 2
-    noStroke();
-    //rotate(this.angle)
-    triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3)
+    fill(this.color.R, this.color.G, this.color.B, 255-255*(this.size/min(windowHeight/20, windowWidth/20)));
+        this.x1 = this.location.x+Math.cos(this.angle+(2*Math.PI/3))*this.size/2
+        this.y1 = this.location.y+Math.sin(this.angle+(2*Math.PI/3))*this.size/2
+        this.x2 = this.location.x+Math.cos(this.angle+2*(2*Math.PI/3))*this.size/2
+        this.y2 = this.location.y+Math.sin(this.angle+2*(2*Math.PI/3))*this.size/2
+        this.x3 = this.location.x+Math.cos(this.angle)*this.size/2
+        this.y3 = this.location.y+Math.sin(this.angle)*this.size/2
+        noStroke();
+        //rotate(this.angle)
+        triangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3)
 
   }
 
   isDead() {
-    if (this.size < 0) {
+    if (this.size < 0 ) {
       return true;
     } else {
       return false;
@@ -154,14 +151,14 @@ class Particle {
 
 class colorGenerator {
   constructor(loc) {
-    this.R = red(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
-    this.G = green(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
-    this.B = blue(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
-  }
+    this.R = red(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+    this.G = green(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+    this.B = blue(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+    }
 
-  update(loc) {
-    this.R = red(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
-    this.G = green(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
-    this.B = blue(imgg.get(floor(loc.x / scale), floor(loc.y / scale)))
+    update(loc) {
+      this.R = red(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+      this.G = green(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+      this.B = blue(imgg.get(floor(loc.x/scale), floor(loc.y/scale)))
+    }
   }
-}
